03aac33c7365c54abebb98ba4fc7acb218b292fa
akabishau <aleksey.kabishau@icloud.com>
2022-12-03 22:20:43 -0500

    feat: configure tab bar controller with 2 generic view controllers

--- AppStoreClone/MainTabBarVC.swift
+++ AppStoreClone/MainTabBarVC.swift
@@ -12,6 +12,27 @@ class MainTabBarVC: UITabBarController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		view.backgroundColor = .green
+		view.backgroundColor = .white
+
+		viewControllers = [
+			createNavController(viewController: UIViewController(), title: "Apps", imageName: "house"),
+			createNavController(viewController: UIViewController(), title: "Search", imageName: "magnifyingglass")
+		]
+	}
+	
+	
+	fileprivate func createNavController(viewController: UIViewController, title: String, imageName: String) -> UIViewController {
+		
+		let navigationController = UINavigationController(rootViewController: viewController)
+		navigationController.tabBarItem.title = title
+		navigationController.tabBarItem.image = UIImage(systemName: imageName)
+		navigationController.navigationBar.prefersLargeTitles = true
+		
+		viewController.navigationItem.title = title
+		viewController.view.backgroundColor = .clear
+		
+		
+		return navigationController
+		
 	}
 }
