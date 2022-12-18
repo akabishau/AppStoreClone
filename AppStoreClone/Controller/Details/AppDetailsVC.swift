dc9bf6c2616d28bb1fb4db72f41fe0a1f62d58b5
akabishau <aleksey.kabishau@icloud.com>
2022-12-17 22:45:45 -0500

    feat: work on displaying screenshot images inside custom preview cell * add custom preview cell and configure it appearance inside details collection view * add preview controller to the preview cell * add collection view with screenshot cell to display screenshots based on number of image urls fetched for each app

--- AppStoreClone/Controller/Details/AppDetailsVC.swift
+++ AppStoreClone/Controller/Details/AppDetailsVC.swift
@@ -39,6 +39,7 @@ class AppDetailsVC: BaseListVC {
 		
 		collectionView.backgroundColor = .systemBackground
 		collectionView.register(AppDetailCell.self, forCellWithReuseIdentifier: AppDetailCell.reuseId)
+		collectionView.register(Previewcell.self, forCellWithReuseIdentifier: Previewcell.reuseId)
 	}
 }
 
@@ -46,14 +47,22 @@ class AppDetailsVC: BaseListVC {
 extension AppDetailsVC {
 	
 	override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-		return 1
+		return 2
 	}
 	
+	
 	override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-		
-		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AppDetailCell.reuseId, for: indexPath) as! AppDetailCell
-		cell.appDetails = appDetails
-		return cell
+		if indexPath.item  == 0 {
+			let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AppDetailCell.reuseId, for: indexPath) as! AppDetailCell
+			cell.appDetails = appDetails
+			return cell
+		} else if indexPath.item == 1 {
+			let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Previewcell.reuseId, for: indexPath) as! Previewcell
+			cell.previewVC.appDetails = appDetails
+			return cell
+		} else {
+			return UICollectionViewCell()
+		}
 	}
 }
 
@@ -62,16 +71,22 @@ extension AppDetailsVC: UICollectionViewDelegateFlowLayout {
 	
 	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
 		
-		var cellHeight: CGFloat
-		// cell auto-size calculation
-		let dummyCell = AppDetailCell(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 1000)) // 1000 is arbitrary number
-		dummyCell.appDetails = appDetails
-		dummyCell.layoutIfNeeded()
-		
-		let estimatedSize = dummyCell.systemLayoutSizeFitting(.init(width: view.frame.width, height: 1000))
-//		print("Auto-sizing: \(estimatedSize)")
-		cellHeight = estimatedSize.height
-		
-		return .init(width: view.frame.width, height: cellHeight)
+		if indexPath.item == 0 {
+			var cellHeight: CGFloat
+			// cell auto-size calculation
+			let dummyCell = AppDetailCell(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 1000)) // 1000 is arbitrary number
+			dummyCell.appDetails = appDetails
+			dummyCell.layoutIfNeeded()
+			
+			let estimatedSize = dummyCell.systemLayoutSizeFitting(.init(width: view.frame.width, height: 1000))
+			//		print("Auto-sizing: \(estimatedSize)")
+			cellHeight = estimatedSize.height
+			
+			return .init(width: view.frame.width, height: cellHeight)
+		} else if indexPath.item == 1 {
+			return .init(width: view.frame.width, height: 400)
+		} else {
+			return .init(width: view.frame.width, height: 100)
+		}
 	}
 }
