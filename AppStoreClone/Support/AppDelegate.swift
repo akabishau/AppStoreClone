53d00c6960a0c79359833bcdee81b24ae2eb3767
akabishau <aleksey.kabishau@icloud.com>
2022-12-25 23:11:09 -0500

    refactor: add back previously removed scene delegate with the default configuration

--- AppStoreClone/Support/AppDelegate.swift
+++ AppStoreClone/Support/AppDelegate.swift
@@ -15,10 +15,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		
-		window = UIWindow(frame: UIScreen.main.bounds)
-		window?.makeKeyAndVisible()
-		window?.rootViewController = MainTabBarVC()
 		return true
 	}
+	
+	
+	// MARK: UISceneSession Lifecycle
+	
+	func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+		
+		return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+	}
+
 }
 
